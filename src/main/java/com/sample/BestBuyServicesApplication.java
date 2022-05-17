e22df07ba9e23a5020c6ebc0a010df5ae20d32d3
BukkaRamesh1 <105722904+BukkaRamesh1@users.noreply.github.com>
2022-05-17 08:50:19 -0400

    Add files via upload
    commit

---
+++ src/main/java/com/sample/BestBuyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBuyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestBuyServicesApplication.class, args);
+	}
+
+}
