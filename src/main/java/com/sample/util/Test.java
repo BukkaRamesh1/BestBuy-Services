0ecd6c8cf866df94e129a84bd4df8b594c931cd5
Nassim Amiri <nassimamiri29@gmail.com>
2022-06-06 02:34:34 -0400

    data type

--- src/main/java/com/sample/util/Test.java
+++ src/main/java/com/sample/util/Test.java
@@ -10,6 +10,20 @@ package com.sample.util;
 public class Test {
 
 	public static void main(String[] args) {
+		byte num1 = -128;
+		short num2 = 13255;
+		int num3 = 5456985;
+		long num4 = 15254455248552648L;
 		
+		//decimals
+		float num5 = 1.2548655465416455624896789955478554741586554522F;
+		double num6 = 100.54585414787858555232154565322145656241458444;
+		
+		//character
+		char c = '$';
+		
+		//true false
+		boolean t = true;
+		boolean f = false;
 	}
 }
